a5cc378ab0f4b8700873661e53011439b7d06d21
ziemianek <mziemianek3@gmail.com>
2023-06-23 21:59:49 +0200

    Remove comments

--- sql_gen/settings.py
+++ sql_gen/settings.py
@@ -21,10 +21,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = credentials.SECRET_KEY
 
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
