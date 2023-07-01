d414342012ba3273938e0b1373c054b0ba7437fd
ziemianek <mziemianek3@gmail.com>
2023-07-01 15:26:50 +0200

    Add session timeout, fix some things

--- sql_gen/views.py
+++ sql_gen/views.py
@@ -1,9 +1,8 @@
-from typing import Any
-from django.db.models.query import QuerySet
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic import CreateView, ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import login
 from django.urls import reverse_lazy
 from generate.models import History
 
@@ -14,14 +13,28 @@ def home_view(request):
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')
     template_name = 'registration/signup.html'
+
+    def form_valid(self, form):
+        response = super().form_valid(form)
+        user = form.save()
+        login(self.request, user)
+        return response
+
+    def get_success_url(self):
+        # Get the 'next' URL parameter from the request's GET data
+        next_url = self.request.GET.get('next')
+        if next_url:
+            return next_url
+        else:
+            return reverse_lazy('home-view')  # Default URL if 'next' parameter is not provided
+
+
 
 
 class HistoryView(LoginRequiredMixin, ListView):
     model = History
     template_name = 'history.html'
-    paginate_by = 15
 
     def get_queryset(self):
         return History.objects.filter(
